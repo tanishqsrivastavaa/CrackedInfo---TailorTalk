64cfa0de5a3a162bc04e54283795187c859fe924
tanishqsrivastavaa <tanishq280205@gmail.com>
2026-05-15 00:56:51 +0530

    Made changes to .gitignore, config.py, and the rest is miscellaneous.
    Signed-off-by: tanishqsrivastavaa <tanishq280205@gmail.com>

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,14 +1,14 @@
 from pydantic_settings import BaseSettings
+from pydantic_settings.main import SettingsConfigDict
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     APP_NAME: str
     ENVIRONMENT: str
+    SERVICE_SECRET: str
 
-
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
