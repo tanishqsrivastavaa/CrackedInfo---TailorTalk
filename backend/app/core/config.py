a8747ba8a729443313441484ad0b42a42378be33
tanishqsrivastavaa <tanishq280205@gmail.com>
2026-05-14 23:49:04 +0530

    Initial Commit + Backend Directory setup.
    Signed-off-by: tanishqsrivastavaa <tanishq280205@gmail.com>

---
+++ backend/app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    APP_NAME: str
+    ENVIRONMENT: str
+
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from crackedinfo!")
+
+
+if __name__ == "__main__":
+    main()
